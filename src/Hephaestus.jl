24ce0cf0faafec7b90c6ff97790ca437b51c9272
Damir Akchurin <AkchurinDA@gmail.com>
2024-11-30 15:46:28 -0500

    - Start from a clean slate

--- src/Hephaestus.jl
+++ src/Hephaestus.jl
@@ -1,30 +1,3 @@
 module Hephaestus
-using OrderedCollections
-using LinearAlgebra
-using StyledStrings
 
-using DocStringExtensions
-
-include("Nodes.jl")
-export AbstractNode, Node
-include("Materials.jl")
-export AbstractMaterial, Material
-include("Sections.jl")
-export AbstractSection, Section
-include("Elements.jl")
-export AbstractElement, Element
-include("Models.jl")
-export AbstractModel, Model
-export add_node!, add_material!, add_section!, add_element!, add_support!, add_conc_load!, add_dist_load!
-export get_node_u_g, get_element_u_l, get_element_f_l
-include("Analysis.jl")
-export AbstractAnalysisType, AbstractAnalysisCache
-export LinearElasticAnalysis, LinearElasticAnalysisCache
-export NonlinearElasticAnalysis, NonlinearElasticAnalysisCache
-export ElasticBucklingAnalysis, ElasticBucklingAnalysisCache
-export FreeVibrationAnalysis, FreeVibrationAnalysisCache
-export solve
-include("Plotting.jl")
-export plotmodel, plotmodel!
-export plotsolution, plotsolution!
 end
